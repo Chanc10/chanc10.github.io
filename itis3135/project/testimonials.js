4ec2c557d86243a0c443c073ae805e92bcff8f9e
Chanc10 <ccrisp10@uncc.edu>
2022-05-07 22:12:53 -0400

    .

---
+++ itis3135/project/testimonials.js
@@ -0,0 +1,9 @@
+// From https://woocommerce.com/flexslider/
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        randomize: true,
+    });
+});
